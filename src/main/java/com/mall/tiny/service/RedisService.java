945453a72855674100efa60533eee40e98c0a09e
fguby <fguby1995@gmail.com>
2019-06-17 15:40:11 +0800

    添加redis操作service

---
+++ src/main/java/com/mall/tiny/service/RedisService.java
@@ -0,0 +1,34 @@
+package com.mall.tiny.service;
+
+/**
+ * redis操作service
+ * 对象和数组都以json形式存储
+ */
+public interface RedisService {
+
+    /**
+     * 存储数据
+     */
+    void set(String key, String value);
+
+    /**
+     * 获取数据
+     */
+    String get(String key);
+
+    /**
+     * 设置过期时间
+     */
+    boolean expire(String key, long expire);
+
+    /**
+     * 删除数据
+     */
+    void remove(String key);
+
+    /**
+     * 自增操作
+     * @param delta 自增步长
+     */
+    Long increment(String key, long delta);
+}
