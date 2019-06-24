df47cba75b7c3e0f240f3fa8f84fc65aeff87b6b
fguby <fguby1995@gmail.com>
2019-06-24 15:56:14 +0800

    添加注释

--- src/main/java/com/mall/tiny/TinyApplication.java
+++ src/main/java/com/mall/tiny/TinyApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动方法
+ */
 @SpringBootApplication
 @MapperScan("com.mall.tiny.mbg.mapper")
 public class TinyApplication {
